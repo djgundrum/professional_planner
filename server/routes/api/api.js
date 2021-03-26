6f5be366d4a7365c84866f9d6a637bdcdd470d34
Declan Gundrum <declan.gundrum.17@gmail.com>
2021-03-26 17:20:30 -0500

    Update api.js

--- server/routes/api/api.js
+++ server/routes/api/api.js
@@ -2,7 +2,13 @@ var express = require("express");
 var router = express.Router();
 
 const user_router = require("./user/user");
+const roles_router = require("./roles/roles");
+const events_router = require("./events/events");
+const schedules_router = require("./schedules/schedules");
 
 router.use("/user", user_router);
+router.use("/roles", roles_router);
+router.use("/events", events_router);
+router.use("/schedules", schedules_router);
 
 module.exports = router;
