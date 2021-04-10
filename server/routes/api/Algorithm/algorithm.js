047433d672e09a1e6a6c4379d468e5748297edec
sam-nichols1 <56178149+sam-nichols1@users.noreply.github.com>
2021-04-09 18:19:37 -0700

    Update algorithm.js

--- server/routes/api/Algorithm/algorithm.js
+++ server/routes/api/Algorithm/algorithm.js
@@ -33,7 +33,17 @@ router.post("/", async (req, res) => {
   }
 });
 
-async function testEmpty() {
+async function testEmptyInput() {
+  var constraints = {};
+
+  var employees = {};
+
+  let alg = await algorithm(constraints, employees);
+  return alg;
+}
+
+//Should return empty schedule
+async function testNoEmployees() {
   var constraints = {
     minHrs: 2,
     maxHrs: 5,
@@ -53,6 +63,454 @@ async function testEmpty() {
   };
 
   var employees = {};
+
+  let alg = await algorithm(constraints, employees);
+  return alg;
+}
+
+//Should return employee 1 working first thing Monday
+async function testOneEmployeeNoConflicts() {
+  var constraints = {
+    minHrs: 2,
+    maxHrs: 5,
+    minShift: 2,
+    maxShift: 4,
+    normalShift: 2.5,
+    minEmployees: 2,
+    maxEmployees: 4,
+    prefferedNumberOfEmployees: 3,
+    businessHrs: {
+      m: { start: 10, end: 17.5 },
+      t: { start: 10, end: 17.5 },
+      w: { start: 10, end: 17.5 },
+      r: { start: 10, end: 17.5 },
+      f: { start: 10, end: 17.5 },
+    },
+  };
+
+  var employees = {
+    e0001: {
+      preferredMinHrs: 3,
+      preferredMaxHrs: 6,
+      preferredHrs: 10,
+      conflicts: {
+        m: [],
+        t: [],
+        w: [],
+        r: [],
+        f: [],
+      },
+    },
+  };
+
+  let alg = await algorithm(constraints, employees);
+  return alg;
+}
+
+//Should return E1 working starting 11 on Monday
+async function testOneEmployeeOneHourConflict() {
+  var constraints = {
+    minHrs: 2,
+    maxHrs: 5,
+    minShift: 2,
+    maxShift: 4,
+    normalShift: 2.5,
+    minEmployees: 2,
+    maxEmployees: 4,
+    prefferedNumberOfEmployees: 3,
+    businessHrs: {
+      m: { start: 10, end: 17.5 },
+      t: { start: 10, end: 17.5 },
+      w: { start: 10, end: 17.5 },
+      r: { start: 10, end: 17.5 },
+      f: { start: 10, end: 17.5 },
+    },
+  };
+
+  var employees = {
+    e0001: {
+      preferredMinHrs: 3,
+      preferredMaxHrs: 6,
+      preferredHrs: 10,
+      conflicts: {
+        m: [10, 11],
+        t: [],
+        w: [],
+        r: [],
+        f: [],
+      },
+    },
+  };
+
+  let alg = await algorithm(constraints, employees);
+  return alg;
+}
+
+//Should return E1 working Tuesday and not Monday
+async function testOneEmployeeMondayConflict() {
+  var constraints = {
+    minHrs: 2,
+    maxHrs: 5,
+    minShift: 2,
+    maxShift: 4,
+    normalShift: 2.5,
+    minEmployees: 2,
+    maxEmployees: 4,
+    prefferedNumberOfEmployees: 3,
+    businessHrs: {
+      m: { start: 10, end: 17.5 },
+      t: { start: 10, end: 17.5 },
+      w: { start: 10, end: 17.5 },
+      r: { start: 10, end: 17.5 },
+      f: { start: 10, end: 17.5 },
+    },
+  };
+
+  var employees = {
+    e0001: {
+      preferredMinHrs: 3,
+      preferredMaxHrs: 6,
+      preferredHrs: 10,
+      conflicts: {
+        m: [10, 17.5],
+        t: [],
+        w: [],
+        r: [],
+        f: [],
+      },
+    },
+  };
+
+  let alg = await algorithm(constraints, employees);
+  return alg;
+}
+
+//Should have both employees working early monday
+async function testTwoEmployeesNoConflict() {
+  var constraints = {
+    minHrs: 2,
+    maxHrs: 5,
+    minShift: 2,
+    maxShift: 4,
+    normalShift: 2.5,
+    minEmployees: 2,
+    maxEmployees: 4,
+    prefferedNumberOfEmployees: 3,
+    businessHrs: {
+      m: { start: 10, end: 17.5 },
+      t: { start: 10, end: 17.5 },
+      w: { start: 10, end: 17.5 },
+      r: { start: 10, end: 17.5 },
+      f: { start: 10, end: 17.5 },
+    },
+  };
+
+  var employees = {
+    e0001: {
+      preferredMinHrs: 3,
+      preferredMaxHrs: 6,
+      preferredHrs: 10,
+      conflicts: {
+        m: [],
+        t: [],
+        w: [],
+        r: [],
+        f: [],
+      },
+    },
+    e0002: {
+      preferredMinHrs: 3,
+      preferredMaxHrs: 6,
+      preferredHrs: 10,
+      conflicts: {
+        m: [],
+        t: [],
+        w: [],
+        r: [],
+        f: [],
+      },
+    },
+  };
+
+  let alg = await algorithm(constraints, employees);
+  return alg;
+}
+
+//Should be same as previous test, with both employees working monday
+async function testTwoEmployeesConflictsLateInWeek() {
+  var constraints = {
+    minHrs: 2,
+    maxHrs: 5,
+    minShift: 2,
+    maxShift: 4,
+    normalShift: 2.5,
+    minEmployees: 2,
+    maxEmployees: 4,
+    prefferedNumberOfEmployees: 3,
+    businessHrs: {
+      m: { start: 10, end: 17.5 },
+      t: { start: 10, end: 17.5 },
+      w: { start: 10, end: 17.5 },
+      r: { start: 10, end: 17.5 },
+      f: { start: 10, end: 17.5 },
+    },
+  };
+
+  var employees = {
+    e0001: {
+      preferredMinHrs: 3,
+      preferredMaxHrs: 6,
+      preferredHrs: 10,
+      conflicts: {
+        m: [],
+        t: [],
+        w: [],
+        r: [10, 14],
+        f: [],
+      },
+    },
+    e0002: {
+      preferredMinHrs: 3,
+      preferredMaxHrs: 6,
+      preferredHrs: 10,
+      conflicts: {
+        m: [],
+        t: [],
+        w: [],
+        r: [],
+        f: [12, 13.5],
+      },
+    },
+  };
+
+  let alg = await algorithm(constraints, employees);
+  return alg;
+}
+
+//Should be E1 working Monday and not Tuesday, E2 working Tuesday and not Monday
+async function testTwoEmployeesBothFullDayConflicts() {
+  var constraints = {
+    minHrs: 2,
+    maxHrs: 5,
+    minShift: 2,
+    maxShift: 4,
+    normalShift: 2.5,
+    minEmployees: 2,
+    maxEmployees: 4,
+    prefferedNumberOfEmployees: 3,
+    businessHrs: {
+      m: { start: 10, end: 17.5 },
+      t: { start: 10, end: 17.5 },
+      w: { start: 10, end: 17.5 },
+      r: { start: 10, end: 17.5 },
+      f: { start: 10, end: 17.5 },
+    },
+  };
+
+  var employees = {
+    e0001: {
+      preferredMinHrs: 3,
+      preferredMaxHrs: 6,
+      preferredHrs: 10,
+      conflicts: {
+        m: [],
+        t: [10, 17.5],
+        w: [],
+        r: [],
+        f: [],
+      },
+    },
+    e0002: {
+      preferredMinHrs: 3,
+      preferredMaxHrs: 6,
+      preferredHrs: 10,
+      conflicts: {
+        m: [10, 17.5],
+        t: [],
+        w: [],
+        r: [],
+        f: [12, 13.5],
+      },
+    },
+  };
+
+  let alg = await algorithm(constraints, employees);
+  return alg;
+}
+
+async function testTenEmployeesNoConflicts() {
+  var constraints = {
+    minHrs: 2,
+    maxHrs: 5,
+    minShift: 2,
+    maxShift: 4,
+    normalShift: 2.5,
+    minEmployees: 2,
+    maxEmployees: 4,
+    prefferedNumberOfEmployees: 3,
+    businessHrs: {
+      m: { start: 10, end: 17.5 },
+      t: { start: 10, end: 17.5 },
+      w: { start: 10, end: 17.5 },
+      r: { start: 10, end: 17.5 },
+      f: { start: 10, end: 17.5 },
+    },
+  };
+
+  var employees = {
+    e0001: {
+      preferredMinHrs: 3,
+      preferredMaxHrs: 6,
+      preferredHrs: 10,
+      conflicts: {
+        m: [],
+        t: [],
+        w: [],
+        r: [],
+        f: [],
+      },
+    },
+    e0003: {
+      preferredMinHrs: 3,
+      preferredMaxHrs: 6,
+      preferredHrs: 11,
+      conflicts: { m: [], t: [], w: [], r: [], f: [] },
+    },
+    e0004: {
+      preferredMinHrs: 3,
+      preferredMaxHrs: 6,
+      preferredHrs: 5,
+      conflicts: { m: [], t: [], w: [], r: [], f: [] },
+    },
+    e0005: {
+      preferredMinHrs: 3,
+      preferredMaxHrs: 6,
+      preferredHrs: 5,
+      conflicts: { m: [], t: [], w: [], r: [], f: [] },
+    },
+    e0006: {
+      preferredMinHrs: 3,
+      preferredMaxHrs: 6,
+      preferredHrs: 5,
+      conflicts: { m: [], t: [], w: [], r: [], f: [] },
+    },
+    e0007: {
+      preferredMinHrs: 3,
+      preferredMaxHrs: 6,
+      preferredHrs: 5,
+      conflicts: { m: [], t: [], w: [], r: [], f: [] },
+    },
+    e0008: {
+      preferredMinHrs: 3,
+      preferredMaxHrs: 6,
+      preferredHrs: 5,
+      conflicts: { m: [], t: [], w: [], r: [], f: [] },
+    },
+    e0009: {
+      preferredMinHrs: 3,
+      preferredMaxHrs: 6,
+      preferredHrs: 5,
+      conflicts: { m: [], t: [], w: [], r: [], f: [] },
+    },
+    e0010: {
+      preferredMinHrs: 3,
+      preferredMaxHrs: 6,
+      preferredHrs: 5,
+      conflicts: { m: [], t: [], w: [], r: [], f: [] },
+    },
+  };
+
+  let alg = await algorithm(constraints, employees);
+  return alg;
+}
+
+async function testTenEmployeeOneConflictEach() {
+  var constraints = {
+    minHrs: 2,
+    maxHrs: 5,
+    minShift: 2,
+    maxShift: 4,
+    normalShift: 2.5,
+    minEmployees: 2,
+    maxEmployees: 4,
+    prefferedNumberOfEmployees: 3,
+    businessHrs: {
+      m: { start: 10, end: 17.5 },
+      t: { start: 10, end: 17.5 },
+      w: { start: 10, end: 17.5 },
+      r: { start: 10, end: 17.5 },
+      f: { start: 10, end: 17.5 },
+    },
+  };
+
+  var employees = {
+    e0001: {
+      preferredMinHrs: 3,
+      preferredMaxHrs: 6,
+      preferredHrs: 10,
+      conflicts: {
+        m: [10, 11],
+        t: [],
+        w: [],
+        r: [12, 16],
+        f: [],
+      },
+    },
+    e0003: {
+      preferredMinHrs: 3,
+      preferredMaxHrs: 6,
+      preferredHrs: 11,
+      conflicts: { m: [], t: [11.5, 16.5], w: [], r: [], f: [] },
+    },
+    e0004: {
+      preferredMinHrs: 3,
+      preferredMaxHrs: 6,
+      preferredHrs: 5,
+      conflicts: { m: [12, 13], t: [], w: [], r: [12, 13], f: [12, 13] },
+    },
+    e0005: {
+      preferredMinHrs: 3,
+      preferredMaxHrs: 6,
+      preferredHrs: 5,
+      conflicts: { m: [], t: [11, 17], w: [], r: [11, 17], f: [] },
+    },
+    e0006: {
+      preferredMinHrs: 3,
+      preferredMaxHrs: 6,
+      preferredHrs: 5,
+      conflicts: { m: [14, 15.5], t: [], w: [], r: [14, 15.5], f: [] },
+    },
+    e0007: {
+      preferredMinHrs: 3,
+      preferredMaxHrs: 6,
+      preferredHrs: 5,
+      conflicts: {
+        m: [11, 12],
+        t: [11, 12],
+        w: [11, 12],
+        r: [11, 12],
+        f: [11, 12],
+      },
+    },
+    e0008: {
+      preferredMinHrs: 3,
+      preferredMaxHrs: 6,
+      preferredHrs: 5,
+      conflicts: { m: [13, 14], t: [], w: [], r: [14, 17.5], f: [] },
+    },
+    e0009: {
+      preferredMinHrs: 3,
+      preferredMaxHrs: 6,
+      preferredHrs: 5,
+      conflicts: { m: [], t: [], w: [12, 13], r: [], f: [] },
+    },
+    e0010: {
+      preferredMinHrs: 3,
+      preferredMaxHrs: 6,
+      preferredHrs: 5,
+      conflicts: { m: [], t: [], w: [], r: [], f: [] },
+    },
+  };
 
   let alg = await algorithm(constraints, employees);
   return alg;
